2cfb6f27eb4abebadc97b05d5714934455416dcb
SML <smlbiobot@gmail.com>
2022-12-26 10:15:03 +0800

    Full conversion to vuepress2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
-const {description} = require('../../package')
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+import { searchPlugin } from '@vuepress/plugin-search'
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 
 const autometa_options = {
     site: {
@@ -12,25 +15,64 @@ const autometa_options = {
     canonical_base: 'https://brand.royaleapi.com',
 };
 
-const seo_options = {
+const seo_options = {};
 
-};
+const description = "Branding guidelines for RoyaleAPI"
 
-module.exports = {
-    /**
-     * Ref：https://v1.vuepress.vuejs.org/config/#title
-     */
+export default defineUserConfig({
     title: 'RoyaleAPI Brand',
-    /**
-     * Ref：https://v1.vuepress.vuejs.org/config/#description
-     */
     description: description,
 
-    /**
-     * Extra tags to be injected to the page HTML `<head>`
-     *
-     * ref：https://v1.vuepress.vuejs.org/config/#head
-     */
+    theme: defaultTheme({
+        repo: '',
+        editLinks: false,
+        docsDir: '',
+        editLinkText: '',
+        lastUpdated: false,
+        logo: '/img/royaleapi-logo.png',
+        nav: require('./nav/en.js'),
+        sidebar: [
+            {
+                text: 'Introduction',
+                link: '/',
+            },
+            {
+                text: 'Elements',
+                link: '/guide/name',
+                children: [
+                    '/guide/name',
+                    '/guide/logo',
+                    '/guide/subbrands',
+                    '/guide/color',
+                    '/guide/typography',
+                ]
+            },
+            {
+                text: 'Examples',
+                link: '/examples/season',
+                children: [
+                    '/examples/season',
+                    '/examples/deck_spotlight',
+                ]
+            },
+            {
+                text: 'Terms',
+                link: '/legal/terms',
+            },
+
+
+        ]
+    }),
+
+    plugins: [
+        searchPlugin({
+            // options
+        }),
+        googleAnalyticsPlugin({
+            id: 'G-48L7KQ2BZD',
+        }),
+    ],
+
     head: [
         ['meta', {name: 'theme-color', content: '#2b73ff'}],
         ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
@@ -101,78 +143,5 @@ module.exports = {
         }],
     ],
 
-    /**
-     * Theme configuration, here is the default theme configuration for VuePress.
-     *
-     * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-     */
-    themeConfig: {
-        repo: '',
-        editLinks: false,
-        docsDir: '',
-        editLinkText: '',
-        lastUpdated: false,
-        logo: '/img/royaleapi-logo.png',
-        nav: require('./nav/en.js'),
-        sidebar: [
-            {
-                title: 'Introduction',
-                path: '/',
-            },
-            {
-                title: 'Elements',
-                collapsable: false,
-                children: [
-                    '/guide/name',
-                    '/guide/logo',
-                    '/guide/subbrands',
-                    '/guide/color',
-                    '/guide/typography',
-                ]
-            },
-            {
-                title: 'Examples',
-                collapsable: false,
-                children: [
-                    '/examples/season',
-                    '/examples/deck_spotlight',
-                ]
-            },
-            {
-                title: 'Terms',
-                path: '/legal/terms',
-            },
 
-
-        ]
-    },
-
-    /**
-     * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-     */
-    plugins: [
-        '@vuepress/plugin-back-to-top',
-        '@vuepress/plugin-medium-zoom',
-        [
-            'vuepress-plugin-container',
-            {
-                type: 'quote',
-                defaultTitle: '',
-            },
-        ],
-        [
-            '@vuepress/plugin-google-analytics',
-            {
-                'ga': 'UA-100687612-15' // UA-00000000-0
-            },
-        ],
-        [
-            'sitemap',
-            {
-                hostname: 'https://brand.royaleapi.com'
-            },
-        ],
-        ['autometa', autometa_options],
-        ['seo', seo_options],
-    ]
-}
+})
